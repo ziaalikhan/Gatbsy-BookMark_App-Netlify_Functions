3b0e3143b2e4dbeb25e5d9ca14ad58af56b755e9
zia <ziaalikhan052@gmail.com>
2021-11-08 17:35:21 -0800

    update

--- functions/bookmarklist/bookmarklist.js
+++ functions/bookmarklist/bookmarklist.js
@@ -1,13 +1,13 @@
 const { ApolloServer, gql } = require("apollo-server-lambda");
-// const faunadb = require("faunadb");
-// q = faunadb.query;
-// const dotenv = require("dotenv");
-// dotenv.config();
+const faunadb = require("faunadb");
+q = faunadb.query;
+const dotenv = require("dotenv");
+dotenv.config();
 
-// let adminClient = new faunadb.Client({
-//   secret: process.env.FAUNADB_ADMIN_SECRET,
-//   domain: "db.us.fauna.com",
-// });
+let adminClient = new faunadb.Client({
+  secret: process.env.FAUNADB_ADMIN_SECRET,
+  domain: "db.us.fauna.com",
+});
 
 const typeDefs = gql`
   type Query {
@@ -18,83 +18,65 @@ const typeDefs = gql`
     task: String!
     url: String!
   }
+  type Mutation {
+    addBookMark(task: String!, url: String!): AllBookMarks
+    deleteTodo(id: ID!): String
+  }
 `;
-const todoData = [
-  {
-    id:0 , task: "hello" , url: 'hello.com'
-  },
-  {
-    id:1 , task: "world" , url: 'world.com'
-  },
-  {
-    id:2 , task: "abc" , url: 'hello.com'
-  },
-  {
-    id:3 , task: "xyz" , url: 'hello.com'
-  },
-]
-
-// type Mutation {
-//   addBookMark(task: String!, url: String!): AllBookMarks
-//   deleteTodo(id: ID!): String
-// }
 
 const resolvers = {
   Query: {
-    bookmarks: () => {
-      return todoData;
-    }
-    // bookmarks: async () => {
-    //   try {
-    //     const result = await adminClient.query(
-    //       q.Map(
-    //         q.Paginate(q.Match(q.Index("url"))),
-    //         q.Lambda((x) => q.Get(x))
-    //       )
-    //     );
-    //     // console.log(result.data , '========>>>>.');
-    //     return result.data.map((val) => {
-    //       return {
-    //         id: val.ref.id,
-    //         task: val.data.task,
-    //         url: val.data.url,
-    //       };
-    //     });
-    //   } catch (err) {
-    //     console.log(err);
-    //   }
-    // },
+    bookmarks: async () => {
+      try {
+        const result = await adminClient.query(
+          q.Map(
+            q.Paginate(q.Match(q.Index("url"))),
+            q.Lambda((x) => q.Get(x))
+          )
+        );
+        // console.log(result.data , '========>>>>.');
+        return result.data.map((val) => {
+          return {
+            id: val.ref.id,
+            task: val.data.task,
+            url: val.data.url,
+          };
+        });
+      } catch (err) {
+        console.log(err);
+      }
+    },
   },
-  // Mutation: {
-  //   addBookMark: async (_, { task, url }) => {
-  //     // console.log("data====> ", task, url);
-  //     try {
-  //       const result = await adminClient.query(
-  //         q.Create(q.Collection("Books"), {
-  //           data: {
-  //             task,
-  //             url,
-  //           },
-  //         })
-  //       );
-  //       return result.data;
-  //     } catch (error) {
-  //       console.log(error);
-  //     }
-  //   },
-  //   deleteTodo: async (_, args) => {
-  //     const id = args.id;
-  //     // return console.log(`${id}  id ====>>> `)
-  //     try {
-  //       const result = await adminClient.query(
-  //         q.Delete(q.Ref(q.Collection("Books"), id))
-  //       );
-  //       return console.log(`${result}=====>>>>is Deleted here`);
-  //     } catch (error) {
-  //       console.log("======>>>", error);
-  //     }
-  //   },
-  // },
+  Mutation: {
+    addBookMark: async (_, { task, url }) => {
+      // console.log("data====> ", task, url);
+      try {
+        const result = await adminClient.query(
+          q.Create(q.Collection("Books"), {
+            data: {
+              task,
+              url,
+            },
+          })
+        );
+        return result.data;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    deleteTodo: async (_, args) => {
+      const id = args.id;
+      // return console.log(`${id}  id ====>>> `)
+      try {
+        const result = await adminClient.query(
+          q.Delete(q.Ref(q.Collection("Books"), id))
+        );
+        return console.log(`${result}=====>>>>is Deleted here`);
+      } catch (error) {
+        console.log("======>>>", error);
+      }
+    },
+  },
 };
 
 const server = new ApolloServer({
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,82 +1,80 @@
 import React, { useState } from "react";
 import { useQuery, useMutation } from "@apollo/client";
 import * as styles from "./main.module.css";
-// import Heading from "../components/Heading";
-// import UserInput from "../components/UserInput";
-// import ListDataHeading from "../components/ListData-Heading";
-// import UserCards from "../components/UserCards";
-// import Addbutton from "../components/Addbutton";
-// import Loading from "../components/Loading";
-// import Error from "../components/Error";
+import Heading from "../components/Heading";
+import UserInput from "../components/UserInput";
+import ListDataHeading from "../components/ListData-Heading";
+import UserCards from "../components/UserCards";
+import Addbutton from "../components/Addbutton";
+import Loading from "../components/Loading";
+import Error from "../components/Error";
 import { BOOKMARKDATA } from "../graphql/query";
-// import { ADD_bOOKMARK } from "../graphql/mutation";
+import { ADD_bOOKMARK } from "../graphql/mutation";
 
 export default function Home() {
   // useQuery
-  const {data } = useQuery(BOOKMARKDATA);
+  const { loading, error, data } = useQuery(BOOKMARKDATA);
   console.log(data)
   // use Mutation
-  // const [addBookMark] = useMutation(ADD_bOOKMARK);
+  const [addBookMark] = useMutation(ADD_bOOKMARK);
 
-  // // Input State //
-  // const [task, setTask] = useState("");
-  // const [url, setUrl] = useState("");
+  // Input State //
+  const [task, setTask] = useState("");
+  const [url, setUrl] = useState("");
 
-  // const addData = () => {
+  const addData = () => {
     // if the input have data it will save in the database and Retrive it
   
-    // if ((task, url)) {
-    //   addBookMark({
-    //     variables: {
-    //       task: task,
-    //       url: url,
-    //     },
-    //     refetchQueries: [{ query: BOOKMARKDATA }],
-    //   });
+    if ((task, url)) {
+      addBookMark({
+        variables: {
+          task: task,
+          url: url,
+        },
+        refetchQueries: [{ query: BOOKMARKDATA }],
+      });
       // if the input will be Empty then Alert will be shown
-    // } else {
-    //   alert("The Inputs Values Are Empty Please Fill It First!");
-    // }
+    } else {
+      alert("The Inputs Values Are Empty Please Fill It First!");
+    }
     // after the data is send to database input will be empty
-  //   setTask("");
-  //   setUrl("");
-  // };
+    setTask("");
+    setUrl("");
+  };
 
   // Loader Start
 
-  // if (loading) {
-  //   return <Loading />;
-  // }
+  if (loading) {
+    return <Loading />;
+  }
 
-  // // Error Start
-  // if (error) {
-  //   return <Error />;
-  // }
+  // Error Start
+  if (error) {
+    return <Error />;
+  }
 
   return (
     <div className={styles.main_container}>
       {/* Heading */}
-      {/* <Heading /> */}
+      <Heading />
       {/* Inputs Container */}
       <div className={styles.inputs_container}>
         {/* UserInput*/}
-        {/* <UserInput task={task} setTask={setTask} url={url} setUrl={setUrl} /> */}
+        <UserInput task={task} setTask={setTask} url={url} setUrl={setUrl} />
 
         {/* Send Data Btn */}
-        {/* <Addbutton addBtn={addData} /> */}
+        <Addbutton addBtn={addData} />
         {/* Send Data Btn End*/}
       </div>
       {/* Inputs Container End*/}
 
       {/* BookMark List Container Heading*/}
-      {/* <ListDataHeading /> */}
+      <ListDataHeading />
 
       {/* BookMark List Container Data  */}
 
-      
       <div className={styles.mainCard}>
-      <h2>Testing</h2>
-        {/* {data.bookmarks.map((val , identity) => {
+        {data.bookmarks.map((val , identity) => {
           return (
             <>
               <UserCards
@@ -87,7 +85,7 @@ export default function Home() {
               />
             </>
           );
-        })} */}
+        })}
       </div>
       {/* BookMark List Container Data End  */}
     </div>
